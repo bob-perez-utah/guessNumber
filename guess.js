4618c65b82aeaf0dd504e2a300c9cb1d22eba7dc
bob-perez-utah <bperez@gw.novell.com>
2018-08-19 19:00:03 -0600

    initial commit

--- guess.js
+++ guess.js
@@ -1,13 +1,13 @@
-var count = 1;
 var secretNumber = 4;
 
 var stringGuess = prompt("Guess the number : ");
 var guess = Number(stringGuess);
 
-do {
+
 
       if(guess === secretNumber) {
         alert("You guessed right !");
+
       }
 
       else if (guess > secretNumber) {
@@ -19,4 +19,3 @@ do {
 
       }
 
-    } while (guess !== secretNumber);
